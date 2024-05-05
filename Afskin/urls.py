9e491aba5e5b58c20a2f4a675d331c76f1cab02b
afnikaulann <kaulanafni@gmail.com>
2024-05-05 19:30:31 +0800

    Update

--- Afskin/urls.py
+++ Afskin/urls.py
@@ -5,7 +5,11 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('product/', views.product, name='product_list'),  # Mengubah categories menjadi products
     path('blog/', views.blog, name='blog'),
-    path('pages/', views.page_list, name='page'),
+    path('blog/article1/', views.article1, name='article1'),
+    path('blog/article2/', views.article2, name='article2'),
+    path('blog/article3/', views.article3, name='article3'),
+    path('blog/article4/', views.article4, name='article4'),
+    path('pages/', views.page_list, name='pages'),
     path('post/<slug:slug>/', views.post_detail, name='post'),
     path('page/<slug:slug>/', views.page_detail, name='page'),
 ]
--- Afskin/views.py
+++ Afskin/views.py
@@ -4,24 +4,36 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product, Blog, Page
 
 def home(request):
-    return render(request, 'blog/home.html')
+    return render(request, 'home.html')
 
 def product(request):
     products = Product.objects.all()  # Mengubah categories menjadi products
-    return render(request, 'blog/product_list.html', {'products': products})
+    return render(request, 'product_list.html', {'products': products})
 
 def blog(request):
     blogs = Blog.objects.filter(status=1).order_by('-published_date')
-    return render(request, 'blog/blog.html', {'blogs': blogs})
+    return render(request, 'blog.html', {'blogs': blogs})
+
+def article1(request):
+    return render(request, 'article1.html')
+
+def article2(request):
+    return render(request, 'article2.html')
+
+def article3(request):
+    return render(request, 'article3.html')
+
+def article4(request):
+    return render(request, 'article4.html')
 
 def page_list(request):
     pages = Page.objects.all()
-    return render(request, 'blog/page_list.html', {'pages': pages})
+    return render(request, 'page_list.html', {'pages': pages})
 
 def post_detail(request, slug):
     post = get_object_or_404(Blog, slug=slug)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'post_detail.html', {'post': post})
 
 def page_detail(request, slug):
     page = get_object_or_404(Page, slug=slug)
-    return render(request, 'blog/page_detail.html', {'page': page})
+    return render(request, 'page_detail.html', {'page': page})
